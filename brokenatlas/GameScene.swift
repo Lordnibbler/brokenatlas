26e69e56326d7137a61eef5b75ad13244354009d
Ben Radler <ben@benradler.com>
2020-06-29 00:27:17 -0700

    Initial commit

--- brokenatlas/GameScene.swift
+++ brokenatlas/GameScene.swift
@@ -9,81 +9,30 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    // using SKTextureAtlas, loading 2.png, on a real device, we see the SKPhysicsBody blue outline does not match the sprite image.
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        physicsWorld.gravity = .zero
+
+        let sprite = SKSpriteNode()
+        sprite.size = CGSize(width: 50, height: 50)
+        sprite.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        let atlas = SKTextureAtlas(named: "myatlas")
+        sprite.texture = atlas.textureNamed("2")
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, alphaThreshold: 0.0, size: sprite.size)
+        self.addChild(sprite)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
+    // using SKTexture, loading the exact same image here, named mysprite.png, on a real device, we see the SKPhysicsBody blue outline exactly matches the sprite image
+//    override func didMove(to view: SKView) {
+//        physicsWorld.gravity = .zero
+//
+//        let sprite = SKSpriteNode()
+//        sprite.size = CGSize(width: 50, height: 50)
+//        sprite.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+//        sprite.texture = SKTexture(imageNamed: "mysprite")
+//        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, alphaThreshold: 0.0, size: sprite.size)
+//        self.addChild(sprite)
+//    }
 }
